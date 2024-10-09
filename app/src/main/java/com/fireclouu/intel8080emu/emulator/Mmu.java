a35fea32639691db7684d8e46b13f7d48fc8d3dd
fireclouu <frcl@frcl.online>
2024-10-09 22:30:07 +0800

    Mmu prevent writing to ROM memory map

--- app/src/main/java/com/fireclouu/intel8080emu/emulator/Mmu.java
+++ app/src/main/java/com/fireclouu/intel8080emu/emulator/Mmu.java
@@ -1,23 +1,30 @@
 package com.fireclouu.intel8080emu.emulator;
-
-import com.fireclouu.intel8080emu.emulator.baseclass.StringUtils;
 import com.fireclouu.intel8080emu.HostHook;
 
 public class Mmu {
     private static final int SP_MEM_ADDR_HI_SCORE_MSB = 0x20f5;
     private static final int SP_MEM_ADDR_HI_SCORE_LSB = 0x20f4;
-
+	
+	private Guest guest;
     private boolean isInitialHiscoreInjected;
     private boolean readHiscoreMsb;
     private boolean readHiscoreLsb;
 
     private HostHook hostHook;
-
-    private short[] memory;
     private boolean isTestSuite;
 
-    public Mmu() {
-        init();
+    public Mmu(Guest guest) {
+        init(guest);
+    }
+	
+	public void init(Guest guest) {
+        isInitialHiscoreInjected = false;
+        readHiscoreMsb = false;
+        readHiscoreLsb = false;
+		
+		this.guest = guest;
+        this.hostHook = HostHook.getInstance();
+        isTestSuite = hostHook.getPlatform().isTestSuite();
     }
 
     private short interceptValue(int address, short value) {
@@ -55,38 +62,26 @@ public class Mmu {
     // tests suite patches
     public void writeTestSuitePatch() {
         // run only if rom has loaded on memory
-        if (memory == null || memory.length <= 0) return;
-        writeMemory(0x0000, (short) 0xD3);
-        writeMemory(0x0001, (short) 0x00);
-        writeMemory(0x0005, (short) 0xDB);
-        writeMemory(0x0006, (short) 0x00);
-        writeMemory(0x0007, (short) 0xC9);
+        if (guest.getMemory() == null || guest.getMemory().length <= 0) return;
+        writeMemoryRomSafe(0x0000, (short) 0xD3);
+        writeMemoryRomSafe(0x0001, (short) 0x00);
+        writeMemoryRomSafe(0x0005, (short) 0xDB);
+        writeMemoryRomSafe(0x0006, (short) 0x00);
+        writeMemoryRomSafe(0x0007, (short) 0xC9);
     }
 
-    public void init() {
-        isInitialHiscoreInjected = false;
-        readHiscoreMsb = false;
-        readHiscoreLsb = false;
-
-        this.hostHook = HostHook.getInstance();
-        memory = new short[StringUtils.Component.PROGRAM_LENGTH];
-        isTestSuite = hostHook.getPlatform().isTestSuite();
+    public void writeMemoryRomSafe(int address, short value) {
+		if (address < 0x1FFF) return;
+		writeMemory(address, value);
+        
     }
-
-    public void writeMemory(int address, short value) {
-        if (!isTestSuite) value = interceptValue(address, value);
-        memory[address & 0xffff] = value;
-    }
+	
+	public void writeMemory(int address, short value) {
+		if (!isTestSuite) value = interceptValue(address, value);
+        guest.getMemory()[address & 0xffff] = value;
+	}
 
     public short readMemory(int address) {
-        return memory[address & 0xffff];
-    }
-
-    public short[] getMemory() {
-        return memory;
-    }
-
-    public void setMemory(short[] memory) {
-        this.memory = memory;
+        return guest.getMemory()[address & 0xffff];
     }
 }
