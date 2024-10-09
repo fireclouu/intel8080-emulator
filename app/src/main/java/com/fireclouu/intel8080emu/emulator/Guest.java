5f6f65056399f5df6ffc2d7af0943c8d2d873d40
fireclouu <frcl@frcl.online>
2024-10-09 22:28:19 +0800

    refactor media id
    code refactors and optimize readability

--- app/src/main/java/com/fireclouu/intel8080emu/emulator/Guest.java
+++ app/src/main/java/com/fireclouu/intel8080emu/emulator/Guest.java
@@ -1,12 +1,50 @@
 package com.fireclouu.intel8080emu.emulator;
+import java.util.LinkedHashMap;
 
 public class Guest {
+	public enum MEDIA_AUDIO {
+		FIRE (0),
+		PLAYER_EXPLODED (1),
+		SHIP_INCOMING (2),
+		ALIEN_MOVE_1 (3),
+		ALIEN_MOVE_2 (3),
+		ALIEN_MOVE_3 (3),
+		ALIEN_MOVE_4 (3),
+		ALIEN_KILLED (0),
+		SHIP_HIT (1);
+		
+		private int id;
+		MEDIA_AUDIO(int id) {
+			this.id = id;
+		}
+		
+		public int getId() {
+			return id;
+		}
+		
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
+	public static LinkedHashMap<String, Integer> mapFileData;
+	
+	public static final int VRAM_START = 0x2400;
+    public static final int VRAM_END = 0x3FFF;
+	public static final int MAX_PROGRAM_SIZE = 0x10_000;
     private final Cpu cpu;
     private final Mmu mmu;
+	private short[] memory;
 
     public Guest() {
-        this.mmu = new Mmu();
+		mapFileData = new LinkedHashMap<>();
+		mapFileData.put("invaders.h", 0x0000);
+		mapFileData.put("invaders.g", 0x0800);
+		mapFileData.put("invaders.f", 0x1000);
+		mapFileData.put("invaders.e", 0x1800);
+		
+        this.mmu = new Mmu(this);
         this.cpu = new Cpu(mmu);
+		memory = new short[MAX_PROGRAM_SIZE];
     }
 
     public Cpu getCpu() {
@@ -15,5 +53,9 @@ public class Guest {
 
     public Mmu getMmu() {
         return this.mmu;
+    }
+	
+	public short[] getMemory() {
+        return memory;
     }
 }
