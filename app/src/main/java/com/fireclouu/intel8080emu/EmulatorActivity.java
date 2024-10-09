86617d050c45d76970581d466c39bef770cd6258
fireclouu <frcl@frcl.online>
2024-10-09 22:22:37 +0800

    code refactors

--- app/src/main/java/com/fireclouu/intel8080emu/EmulatorActivity.java
+++ app/src/main/java/com/fireclouu/intel8080emu/EmulatorActivity.java
@@ -6,33 +6,35 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-
-import com.fireclouu.intel8080emu.emulator.baseclass.StringUtils;
+import android.widget.RelativeLayout; 
 import com.fireclouu.intel8080emu.emulator.KeyInterrupts;
+import android.widget.*;
+import android.graphics.*;
 
 public class EmulatorActivity extends Activity implements Button.OnTouchListener, Button.OnClickListener {
-    Display mDisplay;
+    Display display;
     Platform platform;
-
+	
     // Buttons
-    private Button mButtonCoin, mButtonP1Start, mButtonP1Left, mButtonP1Right, mButtonP1Fire, mButtonSetPlayer, mButtonMenu;
+    private Button mButtonCoin, mButtonP1Start, mButtonP1Left, mButtonP1Right, mButtonP1Fire, mButtonSetPlayer, mButtonLogs;
 
     private LinearLayout llLogs;
+	private LinearLayout llLogs2;
+	
     private RelativeLayout rlEmulator;
 
     private boolean isTestSuite;
 
-    private String testRomFileName;
+    private String romFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_emulation);
+        isTestSuite = getIntent().getBooleanExtra(HostHook.INTENT_FILE_IS_TEST_ROM, false);
+        romFileName = getIntent().getStringExtra(HostHook.INTENT_ROM_FILE_NAME);
 
-        isTestSuite = getIntent().getBooleanExtra(StringUtils.INTENT_FILE_IS_TEST_ROM, false);
-        testRomFileName = getIntent().getStringExtra(StringUtils.INTENT_TEST_ROM_FILE_NAME);
-
+		llLogs2 = findViewById(R.id.llLogs2);
         llLogs = findViewById(R.id.llLogs);
         rlEmulator = findViewById(R.id.rlEmulator);
 
@@ -41,21 +43,50 @@ public class EmulatorActivity extends Activity implements Button.OnTouchListener
 
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         if (isTestSuite) llLogs.setLayoutParams(params);
-
-        requestFullscreen();
+		
         init();
         startEmulation();
+    }
+	
+	private void init() {
+		requestFullscreen();
+        display = findViewById(R.id.mainDisplay);
+
+        mButtonCoin = findViewById(R.id.btn_p1_coin);
+        mButtonP1Start = findViewById(R.id.btn_p1_start);
+        mButtonP1Left = findViewById(R.id.btn_p1_left);
+        mButtonP1Fire = findViewById(R.id.btn_p1_fire);
+        mButtonP1Right = findViewById(R.id.btn_p1_right);
+        mButtonSetPlayer = findViewById(R.id.btn_change_player);
+        mButtonLogs = findViewById(R.id.btn_logs);
+
+        mButtonCoin.setOnTouchListener(this);
+        mButtonP1Start.setOnTouchListener(this);
+        mButtonP1Left.setOnTouchListener(this);
+        mButtonP1Fire.setOnTouchListener(this);
+        mButtonP1Right.setOnTouchListener(this);
+        mButtonSetPlayer.setOnClickListener(this);
+        mButtonLogs.setOnClickListener(this);
+    }
+	
+	private void startEmulation() {
+        if (platform == null) {
+            platform = new Platform(this, this, display, isTestSuite);
+            platform.setRomFileName(romFileName);
+        }
+
+        platform.start();
     }
 
     @Override
     protected void onResume() {
-        platform.setPause(false);
+        // platform.setPause(false);
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        platform.setPause(true);
+        // platform.setPause(true);
         super.onPause();
     }
 
@@ -99,42 +130,13 @@ public class EmulatorActivity extends Activity implements Button.OnTouchListener
             mButtonSetPlayer.setText("P" + playerPort);
         }
 
-        if (buttonId == R.id.btn_menu) {
+        if (buttonId == R.id.btn_logs) {
             boolean isVisible = llLogs.getVisibility() == View.VISIBLE;
             int toggledVisibility = isVisible ? View.GONE : View.VISIBLE;
             llLogs.setVisibility(toggledVisibility);
             isVisible = llLogs.getVisibility() == View.VISIBLE;
-            platform.toggleLog(isVisible);
+            platform.toggleLog(isVisible);	
         }
-    }
-
-    private void startEmulation() {
-        if (platform == null) {
-            platform = new Platform(this, this, mDisplay, isTestSuite);
-            platform.setTestRomFileName(testRomFileName);
-        }
-
-        platform.start();
-    }
-
-    private void init() {
-        mDisplay = findViewById(R.id.mainDisplay);
-
-        mButtonCoin = findViewById(R.id.btn_p1_coin);
-        mButtonP1Start = findViewById(R.id.btn_p1_start);
-        mButtonP1Left = findViewById(R.id.btn_p1_left);
-        mButtonP1Fire = findViewById(R.id.btn_p1_fire);
-        mButtonP1Right = findViewById(R.id.btn_p1_right);
-        mButtonSetPlayer = findViewById(R.id.btn_change_player);
-        mButtonMenu = findViewById(R.id.btn_menu);
-
-        mButtonCoin.setOnTouchListener(this);
-        mButtonP1Start.setOnTouchListener(this);
-        mButtonP1Left.setOnTouchListener(this);
-        mButtonP1Fire.setOnTouchListener(this);
-        mButtonP1Right.setOnTouchListener(this);
-        mButtonSetPlayer.setOnClickListener(this);
-        mButtonMenu.setOnClickListener(this);
     }
 
     private void requestFullscreen() {
