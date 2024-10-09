69665c599b2a43fbf969f87b7ac3769072f0b155
fireclouu <frcl@frcl.online>
2024-10-09 22:38:25 +0800

    code refactors

--- app/src/main/java/com/fireclouu/intel8080emu/emulator/base/ResourceAdapter.java
+++ app/src/main/java/com/fireclouu/intel8080emu/emulator/base/ResourceAdapter.java
@@ -1,28 +1,13 @@
-package com.fireclouu.intel8080emu.emulator.baseclass;
+package com.fireclouu.intel8080emu.emulator.base;
 
 public interface ResourceAdapter {
-    void setEffectFire(int id);
-
-    void setEffectPlayerExploded(int id);
-
-    void setEffectShipIncoming(int id);
-
-    void setEffectAlienMove(int id1, int id2, int id3, int id4);
-
-    void setEffectAlienKilled(int id);
-
-    void setEffectShipHit(int id);
 
     void playSound(int id, int loop);
 
-    void reloadResource();
-
     // special stream for looping sfx
-    void playShipFX();
+    abstract void playShipFX();
 
     void releaseShipFX();
-
-    void initShipFX();
 
     /////   SAVED PREFS   //////
     void putPrefs(String name, int value);
