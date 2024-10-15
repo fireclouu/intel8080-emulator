4e3275c880c8a70e131bec5920c85f287b9fb42a
fireclouu <frcl@frcl.online>
2024-10-15 18:31:23 +0800

    Preparing guest display migration to Guest class

--- app/src/main/java/com/fireclouu/intel8080emu/emulator/base/DisplayAdapter.java
+++ app/src/main/java/com/fireclouu/intel8080emu/emulator/base/DisplayAdapter.java
@@ -1,16 +1,6 @@
 package com.fireclouu.intel8080emu.emulator.base;
+import com.fireclouu.intel8080emu.emulator.*;
 
 public interface DisplayAdapter {
-    int DISPLAY_WIDTH = (32 * 8); // 256 (0x2400 to 0x2407 = bit 0 to bit 7)
-    int DISPLAY_HEIGHT = (224);   // 224
-
-    byte ORIENTATION_DEFAULT = 0;
-    byte DRAW_ORIENTATION_PORTRAIT = 1;
-    byte DRAW_ORIENTATION_LANDSCAPE = 1;
-
-    float[] convertVramToFloatPoints(int drawOrientation, short[] memory);
-
     void draw(short[] memory);
-
-    boolean isDrawing();
 }
