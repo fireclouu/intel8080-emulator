9028ad465c27833cf0427294d4dda720fdf925f1
fireclouu <imfireclouu@gmail.com>
2026-07-25 22:05:14 +0800

    build.gradle.kts: use jdk17 on other subprojects too

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,11 @@ tasks.register("terminalRun") {
     group = "platform"
     dependsOn(":platform:terminal:run")
 }
+
+subprojects {
+    plugins.withType<JavaPlugin> {
+        extensions.configure<JavaPluginExtension> {
+            toolchain { languageVersion = JavaLanguageVersion.of(17) }
+        }
+    }
+}
